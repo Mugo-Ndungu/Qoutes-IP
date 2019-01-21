252c5287f961412a0e21ef73d887ef0b8242e644
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-21 09:46:07 +0300

    Add app module.ts to project folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {TimeAgoPipe} from 'time-ago-pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightDirective } from './highlight.directive';
+import { DatesPipe } from './dates.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    TimeAgoPipe,
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    HighlightDirective,
+    DatesPipe,
+    QuoteFormComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
